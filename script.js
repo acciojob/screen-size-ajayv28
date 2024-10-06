b9c0e4223c44c7ea525a4599c8522aee53814a92
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-10-06 21:45:46 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeDisplay = document.getElementById('sizeDisplay');
+    sizeDisplay.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// Initial size update
+updateSize();
+
+// Update size on window resize
+window.addEventListener('resize', updateSize);
